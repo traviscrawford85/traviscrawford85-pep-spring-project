50a570ab6499cd3722c4fa62fd543f315165aaab
Travis Crawford <tcrawford29@gmail.com>
2024-06-24 19:37:00 +0000

    Created AccountRepository, MessageRepository, AccountService, MessageService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Optional<Account> registerAccount(Account account) {
+        return null;
+        
+    }
+
+    public Optional<Account> loginAccount(String username, String password) {
+        return null;
+        
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,51 @@
 package com.example.service;
 
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Optional<Message> createMessage(Message message) {
+        return Optional.of(messageRepository.save(message));
+        
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Optional<Message> getMessageById(Integer messageId) {
+        return messageRepository.findById(messageId);
+        
+    }
+
+    public boolean deleteMessage(Integer messageId) {
+        if (messageRepository.existsById(messageId)) {
+            messageRepository.deleteById(messageId);
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<Message> updateMessage(Integer messageId, String messageText) {
+        return null;
+        
+    }
+
+    public List<Message> getMessagesByUserId(Integer userId) {
+        return null;
+
+    }
+
+
 }
